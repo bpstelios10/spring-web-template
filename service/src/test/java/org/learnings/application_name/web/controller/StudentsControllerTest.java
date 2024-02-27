ae256aab1515e3c6bee5f9a96e5bc001537a53c5
Stelios Anastasakis <bpstelios10@hotmail.com>
2024-02-27 22:11:12 +0200

    [spring-data] Added missing tests

--- service/src/test/java/org/learnings/application_name/web/controller/StudentsControllerTest.java
+++ service/src/test/java/org/learnings/application_name/web/controller/StudentsControllerTest.java
@@ -17,8 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.HttpStatus.OK;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,23 +33,18 @@ class StudentsControllerTest {
             new StudentDTO(UUID.randomUUID(), "att2", 5, Date.from(Instant.now().minus(Duration.ofDays(2)))),
             new StudentDTO(UUID.randomUUID(), "att3", 1, Date.from(Instant.now().minus(Duration.ofDays(30))))
     );
+    private final StudentDTO expectedStudent = dataSource.get(0);
 
     @Test
     void getAllStudents_whenStudentsExist() {
         when(service.getAllStudents()).thenReturn(dataSource);
-        StudentDTO expectedStudent =
-                new StudentDTO(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
 
         ResponseEntity<List<StudentDTO>> responseEntity = controller.getAllStudents();
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
         assertThat(responseEntity.getBody()).hasSize(3);
         assertThat(responseEntity.getBody()).contains(expectedStudent);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
@@ -61,43 +55,32 @@ class StudentsControllerTest {
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
         assertThat(responseEntity.getBody()).hasSize(0);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
     void getStudentByID_whenStudentsExist() {
-        StudentDTO expectedStudent =
-                new StudentDTO(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
-        when(service.getStudentByID(UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"))).thenReturn(Optional.of(expectedStudent));
+        when(service.getStudentByID(expectedStudent.id())).thenReturn(Optional.of(expectedStudent));
 
         ResponseEntity<StudentDTO> responseEntity = controller.getStudentByID("f9734631-6833-4885-93c5-dd41679fc908");
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
         assertThat(responseEntity.getBody()).isEqualTo(expectedStudent);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
     void getStudentByID_whenStudentsEmpty() {
-        when(service.getStudentByID(UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"))).thenReturn(Optional.empty());
+        when(service.getStudentByID(expectedStudent.id())).thenReturn(Optional.empty());
 
         ResponseEntity<StudentDTO> responseEntity = controller.getStudentByID("f9734631-6833-4885-93c5-dd41679fc908");
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        verifyNoMoreInteractions(service);
     }
 
     @Test
     void createStudent_succeeds() {
-        StudentDTO expectedStudent =
-                new StudentDTO(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
         StudentsController.CreateStudentRequestBody requestBody = new StudentsController.CreateStudentRequestBody(
                 expectedStudent.id(), expectedStudent.fullName(), expectedStudent.currentSemester(), expectedStudent.entryDate());
         doNothing().when(service).createStudent(expectedStudent);
@@ -105,5 +88,27 @@ class StudentsControllerTest {
         ResponseEntity<Void> responseEntity = controller.createStudent(requestBody);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        verifyNoMoreInteractions(service);
+    }
+
+    @Test
+    void searchStudentByName_whenStudentsExist() {
+        when(service.searchStudentByName(expectedStudent.fullName())).thenReturn(List.of(expectedStudent));
+
+        ResponseEntity<List<StudentDTO>> responseEntity = controller.searchStudentByName("att1");
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        assertThat(responseEntity.getBody()).containsExactly(expectedStudent);
+        verifyNoMoreInteractions(service);
+    }
+
+    @Test
+    void searchStudentByName_whenStudentsEmpty() {
+        when(service.searchStudentByName(expectedStudent.fullName())).thenReturn(List.of());
+
+        ResponseEntity<List<StudentDTO>> responseEntity = controller.searchStudentByName("att1");
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        verifyNoMoreInteractions(service);
     }
 }
