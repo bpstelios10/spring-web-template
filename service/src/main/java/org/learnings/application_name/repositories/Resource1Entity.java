011ab0bb7324317abda5e2cb23da5a31f110bec5
Stelios Anastasakis <bpstelios10@hotmail.com>
2024-02-29 09:08:44 +0200

    [spring-data-rest] Added data rest to export API directly from entities
    * One repository creates the default API of spring
    * The second overrides the path
    * The third disables exporting any API

---
+++ service/src/main/java/org/learnings/application_name/repositories/Resource1Entity.java
@@ -0,0 +1,24 @@
+package org.learnings.application_name.repositories;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "resource1")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Resource1Entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "attribute1")
+    private String attr1;
+
+    @Column(name = "attribute2")
+    private String attr2;
+}
