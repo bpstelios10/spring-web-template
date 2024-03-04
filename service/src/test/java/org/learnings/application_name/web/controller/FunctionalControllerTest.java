61187ab418c0086f8057ff22cad83ca6b9e9ee2c
Stelios Anastasakis <bpstelios10@hotmail.com>
2024-03-04 12:35:20 +0200

    [functional-controller] Added JsonFormatter to nicer present dates
    * Introduced ResponseModel and RequestModel in rest module, following clean architecture guidelines

--- service/src/test/java/org/learnings/application_name/web/controller/FunctionalControllerTest.java
+++ service/src/test/java/org/learnings/application_name/web/controller/FunctionalControllerTest.java
@@ -38,15 +38,13 @@ class FunctionalControllerTest {
     @Test
     void getAllResource1_whenResourcesExist() {
         when(service.getAllResource1()).thenReturn(dataSource);
-        FunctionalResource expectedResource =
-                new FunctionalResource(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
+        FunctionalController.Resource1ResponseModel expectedResource = new FunctionalController.Resource1ResponseModel(
+                UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
+                "att1",
+                3,
+                Date.from(Instant.parse("2024-02-01T08:15:24.00Z")));
 
-        ResponseEntity<List<FunctionalResource>> allResource1 = controller.getAllResource1();
+        ResponseEntity<List<FunctionalController.Resource1ResponseModel>> allResource1 = controller.getAllResource1();
 
         assertThat(allResource1.getStatusCode()).isEqualTo(OK);
         assertThat(allResource1.getBody()).hasSize(3);
@@ -57,7 +55,7 @@ class FunctionalControllerTest {
     void getAllResource1_whenResourcesEmpty() {
         when(service.getAllResource1()).thenReturn(List.of());
 
-        ResponseEntity<List<FunctionalResource>> allResource1 = controller.getAllResource1();
+        ResponseEntity<List<FunctionalController.Resource1ResponseModel>> allResource1 = controller.getAllResource1();
 
         assertThat(allResource1.getStatusCode()).isEqualTo(OK);
         assertThat(allResource1.getBody()).hasSize(0);
@@ -65,40 +63,38 @@ class FunctionalControllerTest {
 
     @Test
     void getResource1ByID_whenResourcesExist() {
-        FunctionalResource expectedResource =
-                new FunctionalResource(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
+        FunctionalController.Resource1ResponseModel expectedResponseModel = new FunctionalController.Resource1ResponseModel(
+                UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
+                "att1",
+                3,
+                Date.from(Instant.parse("2024-02-01T08:15:24.00Z")));
+        FunctionalResource expectedResource = new FunctionalResource(expectedResponseModel.id(),
+                expectedResponseModel.attr1(), expectedResponseModel.attr2(), expectedResponseModel.attr3());
         when(service.getResource1ByID(UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"))).thenReturn(Optional.of(expectedResource));
 
-        ResponseEntity<FunctionalResource> allResource1 = controller.getResource1ByID("f9734631-6833-4885-93c5-dd41679fc908");
+        ResponseEntity<FunctionalController.Resource1ResponseModel> allResource1 = controller.getResource1ByID("f9734631-6833-4885-93c5-dd41679fc908");
 
         assertThat(allResource1.getStatusCode()).isEqualTo(OK);
-        assertThat(allResource1.getBody()).isEqualTo(expectedResource);
+        assertThat(allResource1.getBody()).isEqualTo(expectedResponseModel);
     }
 
     @Test
     void getResource1ByID_whenResourcesEmpty() {
         when(service.getResource1ByID(UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"))).thenReturn(Optional.empty());
 
-        ResponseEntity<FunctionalResource> allResource1 = controller.getResource1ByID("f9734631-6833-4885-93c5-dd41679fc908");
+        ResponseEntity<FunctionalController.Resource1ResponseModel> allResource1 = controller.getResource1ByID("f9734631-6833-4885-93c5-dd41679fc908");
 
         assertThat(allResource1.getStatusCode()).isEqualTo(OK);
     }
 
     @Test
     void createResource1() {
-        FunctionalResource expectedResource =
-                new FunctionalResource(
-                        UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
-                        "att1",
-                        3,
-                        Date.from(Instant.parse("2024-02-01T08:15:24.00Z"))
-                );
-        FunctionalController.CreateResource1RequestBody requestBody = new FunctionalController.CreateResource1RequestBody(
+        FunctionalResource expectedResource = new FunctionalResource(
+                UUID.fromString("f9734631-6833-4885-93c5-dd41679fc908"),
+                "att1",
+                3,
+                Date.from(Instant.parse("2024-02-01T08:15:24.00Z")));
+        FunctionalController.Resource1RequestModel requestBody = new FunctionalController.Resource1RequestModel(
                 expectedResource.id(), expectedResource.attribute1(), expectedResource.attribute2(), expectedResource.attribute3());
         doNothing().when(service).createResource1(expectedResource);
 
