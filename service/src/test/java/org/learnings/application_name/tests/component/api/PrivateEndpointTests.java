30c3348f98e24ed19eb22a85819699ba65d84841
Stelios Anastasakis <bpstelios10@hotmail.com>
2025-12-24 14:04:18 +0200

    [spring] Upgraded Spring to latest version, 4.0.1
    * Upgraded to latest lombok and micrometer-registry-prometheus

--- service/src/test/java/org/learnings/application_name/tests/component/api/PrivateEndpointTests.java
+++ service/src/test/java/org/learnings/application_name/tests/component/api/PrivateEndpointTests.java
@@ -3,7 +3,7 @@ package org.learnings.application_name.tests.component.api;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
