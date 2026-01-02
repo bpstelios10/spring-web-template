66dc112427c189903abb484ac3781b75424b3197
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-01-02 13:38:52 +0200

    [spring] Merged master to upgrade gradle/ spring, then resolved failures
    * Upgraded postgresql and h2

--- service/src/test/java/org/learnings/application_name/repositories/StudentsRepositoryTest.java
+++ service/src/test/java/org/learnings/application_name/repositories/StudentsRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.sql.Timestamp;
 import java.time.Instant;
