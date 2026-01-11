9f630fbf34dcf9a1aaf60849404fdbe84b23681b
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-01-11 13:30:20 +0200

    [spring-data-h2] Rename module, to merge it to master
    - fixed docker compose on master

--- service/src/main/java/org/learnings/application_name/services/StudentDTO.java
+++
@@ -1,7 +0,0 @@
-package org.learnings.application_name.services;
-
-import java.util.Date;
-import java.util.UUID;
-
-public record StudentDTO(UUID id, String fullName, int currentSemester, Date entryDate) {
-}
--- template-minimum/src/test/java/org/learnings/application_name/ServiceApplicationTest.java
+++ template-minimum/src/test/java/org/learnings/application_name/ServiceApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServiceApplicationTest {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 }
